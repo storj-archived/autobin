d709753a4bc32b4f1b7e30c1dba28d4e59b14f24
littleskunk <jens.heimbuerge@googlemail.com>
2016-05-30 20:35:57 +0800

    restore lost storjshare-gui scripts

---
+++ storjshare-gui/build-linux-binary.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+
+apiurl=https://api.github.com/repos/Storj/storjshare-gui
+
+repository=$(curl -H "Accept: application/json" -H "Authorization: token $gh_token" $apiurl)
+
+repositoryname=$(echo $repository | jq --raw-output ".name")
+repositoryurl=$(echo $repository | jq --raw-output ".html_url")
+releasesurl=$(echo $repository | jq --raw-output ".releases_url")
+releasesurl=${releasesurl//\{\/id\}/}
+pullurl=$(echo $repository | jq --raw-output ".pulls_url")
+pullurl=${pullurl//\{\/number\}/}
+tagurl=$(echo $repository | jq --raw-output ".tags_url")
+
+#get releases and pull requests from github
+releases=$(curl -H "Accept: application/json" -H "Authorization: token $gh_token" $releasesurl)
+pulls=$(curl -H "Accept: application/json" -H "Authorization: token $gh_token" $pullurl)
+tags=$(curl -H "Accept: application/json" -H "Authorization: token $gh_token" $tagurl)
+
+#build binary for pull request
+for ((i=0; i < $(echo $pulls | jq ". | length"); i++)); do
+
+    pullnumber=$(echo $pulls | jq --raw-output ".[$i].number")
+    pullsha=$(echo $pulls | jq --raw-output ".[$i].merge_commit_sha")
+    pullrepository=$(echo $pulls | jq --raw-output ".[$i].head.repo.html_url")
+    pullbranch=$(echo $pulls | jq --raw-output ".[$i].head.ref")
+
+    releasefound=false
+    assetfound=false
+
+    for ((j=0; j < $(echo $releases | jq ". | length"); j++)); do
+
+        releasename=$(echo $releases | jq --raw-output ".[$j].name")
+
+        if [ "$releasename" = "autobin pull request $pullnumber" ]; then
+
+            releasefound=true
+
+            uploadurl=$(echo $releases | jq --raw-output ".[$j].upload_url")
+            uploadurl=${uploadurl//\{?name,label\}/}
+
+            asseturl=$(echo $releases | jq --raw-output ".[$j].assets_url")
+            assets=$(curl -H "Accept: application/json" -H "Authorization: token $gh_token" $asseturl)
+
+            for ((k=0; k < $(echo $assets | jq ". | length"); k++)); do
+
+                assetlabel=$(echo $assets | jq --raw-output ".[$k].label")
+                assetname=$(echo $assets | jq --raw-output ".[$k].name")
+
+                if [ "${assetname: -4}" = ".deb" ]; then
+                    assetstate=$(echo $assets | jq --raw-output ".[$k].state")
+                    if [ "$assetlabel" = "$pullsha.deb" ] && [ "$assetstate" != "new" ]; then
+                        assetfound=true
+                    else
+                        binaryurl=$(echo $assets | jq --raw-output ".[$k].url")
+                        curl -X DELETE -H "Authorization: token $gh_token" $binaryurl
+                    fi
+                fi
+            done
+        fi
+    done
+
+    if [ $releasefound = false ]; then
+        echo create release autobin pull request $pullnumber
+        uploadurl=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: token $gh_token" -X POST -d "{\"tag_name\":\"\",\"name\":\"autobin pull request $pullnumber\",\"draft\":true}" $releasesurl | jq --raw-output ".upload_url")
+        uploadurl=${uploadurl//\{?name,label\}/}
+    fi
+
+    if [ $assetfound = false ]; then
+        mkdir repos
+        cd repos
+
+        rm -rf $repositoryname
+
+        echo $pullrepository
+        echo create and upload binary $pullrepository $pullbranch
+        git clone $pullrepository -b $pullbranch
+        cd $repositoryname
+        
+        # run script as root
+        #npm install --unsafe-perm
+        #npm run release --unsafe-perm
+
+        # run script as normal user
+        npm install
+        npm run release
+
+        cd releases
+
+        filename=$(ls)
+
+        curl -H "Accept: application/json" -H "Content-Type: application/octet-stream" -H "Authorization: token $gh_token" --data-binary "@$filename" "$uploadurl?name=$filename&label=$pullsha.deb"
+    fi
+done
+
+for ((j=0; j < $(echo $releases | jq ". | length"); j++)); do
+
+    releasename=$(echo $releases | jq --raw-output ".[$j].name")
+
+    if [ "$releasename" = "autobin draft release" ]; then
+        assetfound=false
+        asseturl=$(echo $releases | jq --raw-output ".[$j].assets_url")
+        assets=$(curl -H "Accept: application/json" -H "Authorization: token $gh_token" $asseturl)
+        for ((k=0; k < $(echo $assets | jq ". | length"); k++)); do
+
+            assetname=$(echo $assets | jq --raw-output ".[$k].name")
+
+            if [ "${assetname: -4}" = ".deb" ]; then
+                assetstate=$(echo $assets | jq --raw-output ".[$k].state")
+                if [ "$assetstate" = "new" ]; then
+                    binaryurl=$(echo $assets | jq --raw-output ".[$k].url")
+                    curl -X DELETE -H "Authorization: token $gh_token" $binaryurl
+                else
+                    assetfound=true
+                fi
+            fi
+        done
+
+        if [ $assetfound = false ]; then
+
+            uploadurl=$(echo $releases | jq --raw-output ".[$j].upload_url")
+            uploadurl=${uploadurl//\{?name,label\}/}
+
+            # existing build tag or branch
+            targetbranch=$(echo $releases | jq --raw-output ".[$j].target_commitish")
+            targettag=$(echo $releases | jq --raw-output ".[$j].tag_name")
+            if [ "$targettag" != "null" ]; then
+                for ((l=0; l < $(echo $tags | jq ". | length"); l++)); do
+                    tag=$(echo $tags | jq --raw-output ".[$l].name")
+                    if [ "$targettag" = "$tag" ]; then
+                        targetbranch=$targettag
+                    fi 
+                done
+            fi
+                
+            mkdir repos
+            cd repos
+
+            rm -rf $repositoryname
+
+            echo create and upload binary $repositoryurl $targetbranch
+            git clone $repositoryurl -b $targetbranch
+            cd $repositoryname
+
+            # run script as root
+            #npm install --unsafe-perm
+            #npm run release --unsafe-perm
+
+            # run script as normal user
+            npm install
+            npm run release
+            cd releases
+
+            filename=$(ls)
+            curl -H "Accept: application/json" -H "Content-Type: application/octet-stream" -H "Authorization: token $gh_token" --data-binary "@$filename" "$uploadurl?name=$filename"
+        fi
+    fi
+done
+
---
+++ storjshare-gui/build-windows-binary.bat
@@ -0,0 +1,257 @@
+@echo off
+Setlocal EnableDelayedExpansion
+
+set apiurl=https://api.github.com/repos/Storj/storjshare-gui
+
+curl -H "Accept: application/json" -H "Authorization: token !gh_token!" !apiurl! > repository.json
+
+type repository.json | jq --raw-output ".name" > temp.dat
+set /p repositoryname= < temp.dat
+del temp.dat
+
+type repository.json | jq --raw-output ".html_url" > temp.dat
+set /p repositoryurl= < temp.dat
+del temp.dat
+
+type repository.json | jq --raw-output ".releases_url" > temp.dat
+set /p releasesurl= < temp.dat
+set releasesurl=!releasesurl:{/id}=!
+del temp.dat
+
+type repository.json | jq --raw-output ".pulls_url" > temp.dat
+set /p pullurl= < temp.dat
+set pullurl=!pullurl:{/number}=!
+del temp.dat
+
+type repository.json | jq --raw-output ".tags_url" > temp.dat
+set /p tagurl= < temp.dat
+del temp.dat
+
+rem get releases and pull requests from github
+curl -H "Accept: application/json" -H "Authorization: token !gh_token!" !releasesurl! > releases.json
+curl -H "Accept: application/json" -H "Authorization: token !gh_token!" !pullurl! > pulls.json
+curl -H "Accept: application/json" -H "Authorization: token !gh_token!" !tagurl! > tags.json
+
+rem counting releases
+type releases.json | jq ". | length" > temp.dat
+set /p releases= < temp.dat
+del temp.dat
+set /a releases=!releases!-1
+
+rem counting pull request
+type pulls.json | jq ". | length" > temp.dat
+set /p pulls= < temp.dat
+del temp.dat
+set /a pulls=!pulls!-1
+
+for /L %%I in (0, 1, !pulls!) do (
+
+    type pulls.json | jq --raw-output ".[%%I].number" > temp.dat
+    set /p pullnumber= < temp.dat
+    del temp.dat
+
+    type pulls.json | jq --raw-output ".[%%I].merge_commit_sha" > temp.dat
+    set /p pullsha= < temp.dat
+    del temp.dat
+
+    type pulls.json | jq --raw-output ".[%%I].head.repo.html_url" > temp.dat
+    set /p pullrepository= < temp.dat
+    del temp.dat
+
+    type pulls.json | jq --raw-output ".[%%I].head.ref" > temp.dat
+    set /p pullbranch= < temp.dat
+    del temp.dat
+
+    set releasefound="false"
+    set assetfound="false"
+    for /L %%J in (0, 1, !releases!) do (
+
+        type releases.json | jq --raw-output ".[%%J].name" > temp.dat
+        set /p releasename= < temp.dat
+        del temp.dat
+
+        rem search for a release with autobin and pull request number
+        if "!releasename!" == "autobin pull request !pullnumber!" (
+
+            set releasefound="true"
+
+            type releases.json | jq --raw-output ".[%%J].upload_url" > temp.dat
+            set /p uploadurl= < temp.dat
+            set uploadurl=!uploadurl:{?name,label}=!
+            del temp.dat
+
+            type releases.json | jq --raw-output ".[%%J].assets_url" > temp.dat
+            set /p asseturl= < temp.dat
+            del temp.dat
+
+            curl -H "Accept: application/json" -H "Authorization: token !gh_token!" !asseturl! > assets.json
+
+            type assets.json | jq ". | length" > temp.dat
+            set /p assets= < temp.dat
+            del temp.dat
+            set /a assets=!assets!-1
+
+            for /L %%K in (0, 1, !assets!) do (
+                type assets.json | jq --raw-output ".[%%K].label" > temp.dat
+                set /p assetlabel= < temp.dat
+                del temp.dat
+
+                type assets.json | jq --raw-output ".[%%K].name" > temp.dat
+                set /p assetname= < temp.dat
+                del temp.dat
+
+                if "!assetname:~-4!" == ".exe" (
+
+                    type assets.json | jq --raw-output ".[%%K].state" > temp.dat
+                    set /p assetstate= < temp.dat
+                    del temp.dat
+
+                    if !assetlabel! == !pullsha!.exe (
+                        if not "!assetstate!" == "new" (
+                            set assetfound="true"
+                        ) else (
+                            type assets.json | jq --raw-output ".[%%K].url" > temp.dat
+                            set /p binaryurl= < temp.dat
+                            del temp.dat
+                            curl -X DELETE -H "Authorization: token !gh_token!" !binaryurl!
+                        )
+                    ) else (
+                        type assets.json | jq --raw-output ".[%%K].url" > temp.dat
+                        set /p binaryurl= < temp.dat
+                        del temp.dat
+                        curl -X DELETE -H "Authorization: token !gh_token!" !binaryurl!
+                    )
+                )
+            )
+        )
+    )
+    rem create new release if not exists
+    if not !releasefound! == "true" (
+        echo create release autobin pull request !pullnumber!
+        curl -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: token !gh_token!" -X POST -d "{\"tag_name\":\"\",\"name\":\"autobin pull request !pullnumber!\",\"draft\":true}" !releasesurl! | jq --raw-output ".upload_url" > temp.dat
+        set /p uploadurl= < temp.dat
+        set uploadurl=!uploadurl:{?name,label}=!
+        del temp.dat
+        echo !uploadurl!
+    )
+    rem create and upload a new binary if not exists
+    if not !assetfound! == "true" (
+
+        rem Use C:\ to avoid long file path error
+        cd "C:\"
+
+        rem delete old build files
+        rmdir /S /Q !repositoryname!
+
+        echo create and upload binary !pullrepository! !pullbranch!
+        git clone "!pullrepository!" -b "!pullbranch!"
+        cd !repositoryname!
+        cmd /c npm install
+        cmd /c npm run release
+
+        cd releases
+        ren *.exe *.win32.exe
+        for /R %%F in (*win32.exe) do set filename=%%~nxF
+
+        curl -H "Accept: application/json" -H "Content-Type: application/exe" -H "Authorization: token !gh_token!" --data-binary "@!filename!" "!uploadurl!?name=!filename!&label=!pullsha!.exe"
+    )
+)
+
+for /L %%J in (0, 1, !releases!) do (
+
+    type releases.json | jq --raw-output ".[%%J].name" > temp.dat
+    set /p releasename= < temp.dat
+    del temp.dat
+    
+    rem build binaries for new draft release
+    if "!releasename!" == "autobin draft release" (
+
+        set assetfound="false"
+
+        type releases.json | jq --raw-output ".[%%J].assets_url" > temp.dat
+        set /p asseturl= < temp.dat
+        del temp.dat
+
+        curl -H "Accept: application/json" -H "Authorization: token !gh_token!" !asseturl! > assets.json
+
+        type assets.json | jq ". | length" > temp.dat
+        set /p assets= < temp.dat
+        del temp.dat
+        set /a assets=!assets!-1
+
+        for /L %%K in (0, 1, !assets!) do (
+            type assets.json | jq --raw-output ".[%%K].name" > temp.dat
+            set /p assetname= < temp.dat
+            del temp.dat
+
+            if "!assetname:~-4!" == ".exe" (
+
+                type assets.json | jq --raw-output ".[%%K].state" > temp.dat
+                set /p assetstate= < temp.dat
+                del temp.dat
+
+                if "!assetstate!" == "new" (
+                    type assets.json | jq --raw-output ".[%%K].url" > temp.dat
+                    set /p binaryurl= < temp.dat
+                    del temp.dat
+                    curl -X DELETE -H "Authorization: token !gh_token!" !binaryurl!
+                ) else (
+                    set assetfound="true"
+                )
+            )
+        )
+
+        if not !assetfound! == "true" (
+            type releases.json | jq --raw-output ".[%%J].upload_url" > temp.dat
+            set /p uploadurl= < temp.dat
+            set uploadurl=!uploadurl:{?name,label}=!
+            del temp.dat
+
+            type releases.json | jq --raw-output ".[%%J].target_commitish" > temp.dat
+            set /p targetbranch= < temp.dat
+            del temp.dat
+
+            type releases.json | jq --raw-output ".[%%J].tag_name" > temp.dat
+            set /p targettag= < temp.dat
+            del temp.dat
+
+            if not !targettag! == null (
+
+                type tags.json | jq ". | length" > temp.dat
+                set /p tags= < temp.dat
+                del temp.dat
+                set /a tags=!tags!-1
+
+                for /L %%L in (0, 1, !tags!) do (
+
+                    type tags.json | jq --raw-output ".[%%L].name" > temp.dat
+                    set /p tag= < temp.dat
+                    del temp.dat
+
+                    if !targettag! == !tag! (
+                        set targetbranch=!targettag!
+                    )
+                )
+            )
+
+            rem Use C:\ to avoid long file path error
+            cd "C:\"
+
+            rem delete old build files
+            rmdir /S /Q !repositoryname!
+
+            echo create and upload binary !repositoryurl! !targetbranch!
+            git clone !repositoryurl! -b "!targetbranch!"
+            cd !repositoryname!
+            cmd /c npm install
+            cmd /c npm run release
+
+            cd releases
+            ren *.exe *.win32.exe
+            for /R %%F in (*win32.exe) do set filename=%%~nxF
+
+            curl -H "Accept: application/json" -H "Content-Type: application/exe" -H "Authorization: token !gh_token!" --data-binary "@!filename!" "!uploadurl!?name=!filename!" > upload.json
+        )
+    )
+)
+
