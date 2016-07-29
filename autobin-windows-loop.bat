7598ff8eb304eb3dfe76fc501a7bdf741570adde
littleskunk <jens.heimbuerge@googlemail.com>
2016-07-29 19:09:35 +0800

    set code sign file and password

--- autobin-windows-loop.bat
+++ autobin-windows-loop.bat
@@ -3,6 +3,8 @@ rem set some variables
 Setlocal EnableDelayedExpansion
 
 set gh_token=insert your token here
+set CERT_FILE=insert your code cert file here
+set CERT_PASSWORD=insert your cert password here
 
 set workdir=!cd!
 
